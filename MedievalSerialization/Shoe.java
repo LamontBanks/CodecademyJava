a6c5f278a84ad885a8bda27f8fcd0748b845f3f0
Lamont Banks <lamontbanks@gmail.com>
2024-08-02 13:05:33 -0400

    Pre-created files

---
+++ MedievalSerialization/Shoe.java
@@ -0,0 +1,9 @@
+public class Shoe extends Armour {
+
+    /* Class Variables */
+
+    /* Constructors */
+    public Shoe(String name, int durability, int defenseRating) {
+        super(name, durability, defenseRating);
+    }
+}
